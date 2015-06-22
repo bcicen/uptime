8616483376233d72d75b7962b9d50c74580a6c19
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-06-22 11:49:40 -0400

    rename var ETCD_HOST

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 cp /app/uptime/sample_config.py /app/uptime/config.py
 
-if [ -z $ETCD_URL ]; then
+if [ -z "$ETCD_HOST" ]; then
 	etcd &
+	sleep 2
 else
-	ETCD_HOST=$(echo $ETCD_URL | cut -f1 -d\:)
-	ETCD_PORT=$(echo $ETCD_URL | cut -f2 -d\:)
+	ETCD_HOST=$(echo $ETCD_HOST | cut -f1 -d\:)
+	ETCD_PORT=$(echo $ETCD_HOST | cut -f2 -d\:)
 	sed -i "s/localhost/$ETCD_HOST/g" /app/uptime/config.py
 	sed -i "s/4001/$ETCD_PORT/g" /app/uptime/config.py
 fi
