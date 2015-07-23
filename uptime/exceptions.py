acf0399b65dd53731fa29528fdadd41abb387bab
Kevin Schoon <kevinschoon@gmail.com>
2015-07-22 21:51:01 -0400

    remove __author__

--- uptime/exceptions.py
+++ uptime/exceptions.py
@@ -1,5 +1,3 @@
-__author__ = 'kevinschoon@gmail.com'
-
 
 class UptimeError(Exception):
     pass
--- uptime/sample_config.py
+++
@@ -1,8 +0,0 @@
-__version__ = '0.1'
-
-
-class Config(object):
-    # SLACK_URL = ''
-    DEBUG = True
-    REDIS_HOST = 'redis'
-    REDIS_PORT = 6379
